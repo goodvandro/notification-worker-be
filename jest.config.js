bb03ea86266b2bc5d07e38f98dee6107f9c2687b
Evandro Monteiro Lima <goodvandro@gmail.com>
2025-06-03 19:05:51 +0000

    refactor(jest): move jest config from package.json to separate file
    The jest configuration was moved from package.json to a dedicated jest.config.js file for better maintainability and separation of concerns. This follows the common practice of keeping test configurations separate from package configurations.

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',  // aqui vai a config do ts-jest
+      },
+    ],
+  },
+};
