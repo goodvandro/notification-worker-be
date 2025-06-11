a3e6a1807824a92140556906f99aebb976f1d32e
Evandro Monteiro Lima <goodvandro@gmail.com>
2025-06-11 16:49:42 +0000

    refactor(worker): update worker startup command and add mongo module
    - Change worker startup command to use ts-node directly for better compatibility
    - Add MongoModule to WorkerModule imports for database connectivity

--- src/infra/queue/worker.module.ts
+++ src/infra/queue/worker.module.ts
@@ -4,8 +4,9 @@ import { Module } from '@nestjs/common';
 import { MessageModule } from 'src/modules/message/message.module';
 import { BullConfigModule } from './bull-config.module';
 import { QueueModule } from './queue.module';
+import { MongoModule } from '../db/mongodb/mongodb.module';
 
 @Module({
-  imports: [BullConfigModule, QueueModule, MessageModule],
+  imports: [BullConfigModule, MongoModule, QueueModule, MessageModule],
 })
 export class WorkerModule {}
